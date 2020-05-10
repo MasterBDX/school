e6dd42e429e4f54a6e12b1842e2504cac6ceb598
masterbdx <masterbdxteam@gmail.com>
2020-05-10 04:36:55 +0200

    I have Improved The Home Page Styles

--- posts/templatetags/post_filters.py
+++ posts/templatetags/post_filters.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(name='ar_date')
 def get_ar_date(value):
-    date = value.strftime(' %-d / %Y - %-I:%M ')
+    date = value.strftime(' %d / %Y - %I:%M ')
     pm_am = value.strftime('%p')
     if pm_am == 'PM':
         pm_am = 'مساء'
