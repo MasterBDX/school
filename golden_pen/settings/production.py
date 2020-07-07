69ef86c0fe202e3413ad594427d4b4b889cf5361
MasterBDX <masterbdxteam@gmail.com>
2020-07-07 22:46:20 +0200

    i added logging to settings

--- golden_pen/settings/production.py
+++ golden_pen/settings/production.py
@@ -25,34 +25,35 @@ EMAIL_HOST_PASSWORD = 'Baleead9@21112004'
 SERVER_EMAIL = 'assassinbd9@gmail.com'
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-                 '()': 'django.utils.log.RequireDebugFalse',
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            #'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-                },
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
             },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+                      },
+            'filters': {
+                'require_debug_false': {
+                        '()': 'django.utils.log.RequireDebugFalse',
+                }
+            },
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                #'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+                    },
+                },
     'loggers': {
         'django': {
             'handlers': ['file', 'console', 'mail_admins',],
             'propagate': True,
             'level': 'DEBUG',
         },
+        }
     }
 
 DEBUG = False
